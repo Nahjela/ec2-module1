611c9ab6bd4537666a82c0abb04ef1da5ee02b72
Nahjela <nahjela0@gmail.com>
2024-04-28 12:44:22 -0500

    module

---
+++ ec2/ec2.tf
@@ -0,0 +1,7 @@
+module "ec2-server" {
+    source = "../"
+    ami = "ami-0a1179631ec8933d7"
+region_name = "us-east-1"
+profile_name = "default"
+instanceType = "t2.micro"
+    }
--- terraform.tfvars
+++
@@ -1,4 +0,0 @@
-ami = "ami-0a1179631ec8933d7"
-region_name = "us-east-1"
-profile_name = "default"
-instanceType = "t2.micro"
