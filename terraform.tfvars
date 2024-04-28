f70879a8f0155e05867aa52c98e73babcd438364
Nahjela <nahjela0@gmail.com>
2024-04-28 12:27:03 -0500

    terraform

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+ami = "ami-0a1179631ec8933d7"
+region_name = "us-east-1"
+profile_name = "default"
+instanceType = "t2.micro"
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "ami" {
+    description = "Amazon machine image"
+    type = string
+    default = "ami-0a1179631ec8933d7"
+  
+}
+variable "instanceType" {
+    default = "t2.small"
+  
+}
+variable "region_name" {
+    # default = "us-east-1"
+  
+}
+
+variable "profile_name" {
+    default = "default"
+  
+}
